4abe6363da22fce784a6072e30a90ff4b89f0235
Francesc <fguerrero@linear.es>
2024-01-23 08:19:15 +0100

    - Sekisui doesn't change the ITEM_NAME from tables like Calib when that same field is changed in the ITEM_PARA table. When performing joins, it is better to convert the ITEM_NAME to ITEM_NO to avoid this problem.
    - New functions to import reaction curves and calibration replicates from Sekisui

--- R/sekisui.R
+++ R/sekisui.R
@@ -14,7 +14,7 @@ imp_sk_results <- function(conn = connect_to_sk_dbi()){
   results
 }
 
-#' Import calibration data from Sekisui for given methods.
+#' Import current calibration data from Sekisui for given methods.
 #' `r lifecycle::badge('experimental')`
 #'
 #' @param conn a connection object like the one returned from connect_to_sk_dbi
@@ -24,9 +24,14 @@ imp_sk_results <- function(conn = connect_to_sk_dbi()){
 #' @export
 imp_sk_caldata <- function(item_names, conn = connect_to_sk_dbi()){
 
+  item_no <- h_sk_item_name_to_no(item_names)
+
   cal_od <- dplyr::tbl(conn, "Calib") %>%
     dplyr::collect() %>%
-    dplyr::filter(ITEM_NAME %in% item_names, HIST_NO == 5) %>%
+    dplyr::filter(ITEM_NO %in% item_no) %>%
+    dplyr::group_by(ITEM_NO) %>%
+    dplyr::filter(HIST_NO == max(HIST_NO)) %>%
+    dplyr::ungroup() %>%
     dplyr::select(
       ITEM_NO,
       dplyr::ends_with("OD")
@@ -62,14 +67,12 @@ imp_sk_caldata <- function(item_names, conn = connect_to_sk_dbi()){
     ) %>%
     tidyr::drop_na()
 
-
    out <- dplyr::left_join(cal_od, cal_value, by = dplyr::join_by(ITEM_NO, CAL))
 
    class(out) <- c("sk_cal", class(out))
 
    out
 }
-
 
 #' Compute a mean like sekisui SK500 (drop one, mean of the rest)
 #' `r lifecycle::badge('stable')`
@@ -87,3 +90,110 @@ fct_meanSek <- function(x){
   mean(x[x_dif != max( x_dif )])
 
 }
+
+# New ----
+
+#' Convert ITEM_NAME to ITEM_NO for Sekisui SK500 database
+#'
+#' @param item_names
+#' @param conn a connection object like the one returned from connect_to_sk_dbi
+#'
+#' @return an integer vector of ITEM_NO
+#' @export
+h_sk_item_name_to_no <- function(item_names, conn = connect_to_sk_dbi()){
+  dplyr::tbl(conn_sk, "ITEMPARA") %>%
+    dplyr::filter(ITEM_NAME %in% item_names) %>%
+    dplyr::pull(ITEM_NO)
+}
+
+#' Import reaction curves from Sekisui SK500
+#' `r lifecycle::badge('experimental')`
+#'
+#' @param x Describir...
+#'
+#' @return a tibble of class "sk_rc".
+#' @export
+imp_sk_rc <- function(x, ...){
+  UseMethod("imp_sk_rc")
+}
+
+#' @describeIn imp_sk_rc method
+#' @export
+imp_sk_rc.integer <- function(x){
+
+  out <- dplyr::tbl(conn_sk, "RC_OD") %>%
+    dplyr::filter(RC_NO %in% x) %>%
+    dplyr::collect()
+
+  if(FALSE){
+    # For future wrangle
+    out %>%
+      dplyr::rename(cycle = CYCLE, abs1 = COL1_SEL, abs2 = COL2_SEL)
+
+  }
+
+  class(out) <- c("sk_rc", class(out))
+
+  out
+
+}
+
+#' @describeIn imp_sk_rc method
+#' @export
+imp_sk_rc.sk_results <- function(x){
+
+  out <- dplyr::tbl(conn_sk, "RC_OD") %>%
+    dplyr::filter(RC_NO %in% local(x$RC_NO)) %>%
+    dplyr::collect()
+
+  class(out) <- c("sk_rc", class(out))
+
+  out
+
+}
+
+#' Import the calibration replicates from Sekisui SK500's database.
+#' `r lifecycle::badge('experimental')`
+#'
+#' @param x Object of class "sk_cal"
+#' @param conn a connection object like the one returned from connect_to_sk_dbi
+#'
+#' @return a tibble of class "sk_cal_rep"
+#' @export
+imp_sk_calrep <- function(x, conn = connect_to_sk_dbi()){
+
+  stopifnot(inherits(x, "sk_cal"))
+
+  CalData <- dplyr::tbl(conn_sk,  "CalData") %>%
+    dplyr::filter(ITEM_NO %in% local(item_no)) %>%
+    dplyr::collect()
+
+  imp_res <- imp_sk_results(conn)
+
+  out <- x %>%
+    dplyr::left_join(
+      CalData,
+      dplyr::join_by(ITEM_NO == ITEM_NO, ITEM_NAME == ITEM_NAME, OD == OD_DATA)
+    ) %>%
+    dplyr::select(ITEM_NO, ITEM_NAME, RST_DATE, SAMP_NO) %>%
+    dplyr::left_join(
+      CalData,
+      by = dplyr::join_by(ITEM_NO, ITEM_NAME, SAMP_NO, RST_DATE >= RST_DATE)
+    ) %>%
+    dplyr::group_by(ITEM_NO, ITEM_NAME, SAMP_NO) %>%
+    dplyr::arrange(desc(RST_DATE.y)) %>%
+    dplyr::group_modify(.f = \(x, ...) head(x, n = 4)) %>%
+    dplyr::ungroup() %>%
+    dplyr::filter(IS_TOTAL == 0) %>%
+    dplyr::select(
+      ITEM_NO,
+      RDATE = RST_DATE.y
+    ) %>%
+    dplyr::left_join(imp_res, dplyr::join_by(ITEM_NO, RDATE))
+
+  class(out) <- c("sk_cal_rep", "sk_results", class(out))
+
+  out
+}
+
+
